2dd5d5102876922d1b1b09d27a219f62dca75508
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-01 18:11:33 -0300

    N261-262-263-264 - CRUD

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -23,35 +23,87 @@ export const getUser = async( req: Request, res: Response) => {
     }
 }
 
-export const postUser = ( req: Request, res: Response) => {
+export const postUser = async( req: Request, res: Response) => {
 
     const {body} = req;
+
+    try {
+
+        const existsEmail = await User.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if (existsEmail) {
+            return res.status(400).json({
+                msg: 'There is already an user with the email ' + body.email
+            })
+        }
+
+        const user = await User.create(body); 
+
+        res.json({ user })
+        
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({
+            msg: 'Contact the Admin' 
+        })
+
+    }
     
-    res.json({
-        msg: 'postUser',
-        body
-    })
+
 }
 
-export const putUser = ( req: Request, res: Response) => {
+export const putUser = async( req: Request, res: Response) => {
 
     const {id}     = req.params;
     const { body } = req;
     
-    res.json({
-        msg: 'putUser',
-        id,
-        body
-    })
+    try {
+
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(400).json({
+                msg: 'There is no user with the id ' + id
+            })
+        }
+
+        await user.update( body );
+
+        res.json( user );
+        
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({
+            msg: 'Contact the Admin' 
+        })
+
+    }
 }
 
-export const deleteUser = ( req: Request, res: Response) => {
+export const deleteUser = async( req: Request, res: Response) => {
 
     const {id}     = req.params; 
+
+    const user = await User.findByPk(id);
+    if (!user) {
+        return res.status(400).json({
+            msg: 'There is no user with the id ' + id
+        })
+    }
+
+    //☠☠☠ BORRAR FISICAMENTE ☠☠☠
+    //await user.destroy();
+
+    await user.update({status: false});
     
     res.json({
-        msg: 'deleteUser',
-        id
+        msg: 'User deleted',
+        user
     })
 }
 
