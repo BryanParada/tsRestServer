58b3161bed81394a360b358b23a5a7e10cb7e009
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-01 15:39:24 -0300

    N256 - Routes

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.name = void 0;
-exports.name = 'Bryan';
-console.log(exports.name);
-//# sourceMappingURL=app.js.map
--- models/server.ts
+++ models/server.ts
@@ -1,21 +1,31 @@
-import express, {Application} from "express";
-
+import express, {Application} from "express"; 
+import userRoutes from '../routes/user.routes';
 
 class Server {
 
     //private app: express.Application; /7 usar esta sentencia para no importar {Application }^
     private app: Application;
     private port: string;
+    private apiPaths = {
+        //ruta del endpoint
+        users: '/api/users' 
+    }
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8080';
+
+         //Definir rutas
+         this.routes();
+    }
+
+    routes(){
+        this.app.use( this.apiPaths.users, userRoutes)
     }
 
     listen(){
         this.app.listen( this.port, () => {
-            console.log('Server running on port '+ this.port);
-            
+            console.log('Server running on port '+ this.port); 
         });
     }
 }
